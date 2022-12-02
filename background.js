222db7db2ce7b6e2429c4778aeb16220008c6392
StefanSanchezA <stefansanchezalarcon@gmail.com>
2022-12-02 10:19:06 +1000

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-
-    chrome.tabs.sendMessage(tabId, {
-      type: "NEW",
-      videoId: urlParameters.get("v"),
-    });
+  if (tab.url && tab.url.includes("youtube.com/shorts")) {
+    chrome.tabs.update({url: "https://scrimba.com/learn/learnjavascript/deploy-the-final-version-coc8640b4ab7ea074c56f6bdc"});   
+    window.close()
   }
 });
