0bd5d0d0893b1779ed6912644a031652e5bf8247
StefanSanchezA <stefansanchezalarcon@gmail.com>
2022-12-17 14:35:26 +1000

    Update 2: changed link and Icon

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/shorts")) {
-    chrome.tabs.update({url: "https://scrimba.com/learn/learnjavascript/deploy-the-final-version-coc8640b4ab7ea074c56f6bdc"});   
+    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
     window.close()
   }
 });
