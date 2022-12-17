37122a1f76b45766e3b05f0ce498fa7ac5599b3c
StefanSanchezA <stefansanchezalarcon@gmail.com>
2022-12-17 14:51:20 +1000

    removed useless content

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/shorts")) {
     chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
-    window.close()
   }
 });
