9a9b3a7d48137a38375e11c1fb7f994388a991d6
StefanSanchezA <stefansanchezalarcon@gmail.com>
2022-12-30 09:05:58 +1000

    Added Tiktok & Reels elimination

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  //Youtube Shorts
   if (tab.url && tab.url.includes("youtube.com/shorts")) {
+    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  }
+
+  //Tiktok
+  if (tab.url && tab.url.includes("tiktok.com")) {
+    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  }
+
+  //IG Reels
+  if (tab.url && tab.url.includes("https://www.instagram.com/reels")) {
+    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  }
+
+  //FB Reels 
+  if (tab.url && tab.url.includes("https://www.facebook.com/reel")) {
     chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
   }
 });
