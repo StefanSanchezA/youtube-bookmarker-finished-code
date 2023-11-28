c350b33070c738b5b1bbb141a45ae0184ba87d76
StefanSanchezA <stefansanchezalarcon@gmail.com>
2023-11-28 08:17:44 -0400

    updated background.js

--- background.js
+++ background.js
@@ -1,21 +1,22 @@
+
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   //Youtube Shorts
-  if (tab.url && tab.url.includes("youtube.com/shorts")) {
-    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  if (tab.url && tab.url.includes("Youtube")) {
+    chrome.tabs.update({url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});   
   }
 
   //Tiktok
-  if (tab.url && tab.url.includes("tiktok.com")) {
-    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  else if (tab.url && tab.url.includes("tiktok")) {
+    chrome.tabs.update({url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});   
   }
 
   //IG Reels
-  if (tab.url && tab.url.includes("https://www.instagram.com/reels")) {
-    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  else if (tab.url && tab.url.includes("instagram")) {
+    chrome.tabs.update({url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});   
   }
 
   //FB Reels 
-  if (tab.url && tab.url.includes("https://www.facebook.com/reel")) {
-    chrome.tabs.update({url: "https://scrimba.com/learn/frontend"});   
+  else if (tab.url && tab.url.includes("facebook")) {
+    chrome.tabs.update({url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});   
   }
 });
