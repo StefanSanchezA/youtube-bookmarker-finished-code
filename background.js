d946b7d2beaa2c3aa49b9300e3180641cdb70acd
StefanSanchezA <stefansanchezalarcon@gmail.com>
2023-11-28 08:56:01 -0400

    v4

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Only act on loading tabs
   if (changeInfo.status !== 'loading') return;
 
-  // Exact URL match for the exception
-  const exceptionUrl = "https://www.youtube.com/watch?v=yLUqkgpSw7Y&t=39892s&ab_channel=OmachiPham";
-  if (tab.url === exceptionUrl) {
-    return; // Do nothing if the URL matches exactly
+  // Exceptions
+  if (tab.url && tab.url.includes("OmachiPham")) {
+    // No action for this URL
+    return;
   }
-
-  // Redirect for specific sites
-  if (tab.url.includes("youtube") || tab.url.includes("tiktok") || tab.url.includes("instagram") || tab.url.includes("facebook")) {
+  // Youtube Shorts, Tiktok, IG Reels, FB Reels
+  else if (tab.url && (tab.url.includes("youtube") || tab.url.includes("tiktok") || tab.url.includes("instagram") || tab.url.includes("facebook"))) {
     chrome.tabs.update(tabId, {url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});
   }
 });
