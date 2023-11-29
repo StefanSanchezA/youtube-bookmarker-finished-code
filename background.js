d302b19b27fb35b975668c21ee60859ad9060d03
StefanSanchezA <stefansanchezalarcon@gmail.com>
2023-11-29 10:04:01 -0400

    v5

--- background.js
+++ background.js
@@ -2,14 +2,16 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Only act on loading tabs
   if (changeInfo.status !== 'loading') return;
+  if(tab.url){
 
-  // Exceptions
-  if (tab.url && tab.url.includes("OmachiPham")) {
-    // No action for this URL
-    return;
-  }
-  // Youtube Shorts, Tiktok, IG Reels, FB Reels
-  else if (tab.url && (tab.url.includes("youtube") || tab.url.includes("tiktok") || tab.url.includes("instagram") || tab.url.includes("facebook"))) {
-    chrome.tabs.update(tabId, {url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});
-  }
+    // Exceptions
+    if (tab.url.includes("OmachiPham")) {
+      // No action for this URL
+      return;
+    }
+    // Youtube, Tiktok, IG, FB
+    else if ((tab.url.includes("youtube.com") || tab.url.includes("tiktok") || tab.url.includes("instagram") || tab.url.includes("facebook"))) {
+      chrome.tabs.update(tabId, {url: "https://calendar.google.com/calendar/u/0/r?tab=rc"});
+    }}
+
 });
